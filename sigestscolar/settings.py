64514f7a3165adb3d07e55e639fdbf4e4b41af0e
christian <chrisstianandres@gmail.com>
2021-12-24 18:39:34 -0500

    seet

--- sigestscolar/settings.py
+++ sigestscolar/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#0!acpa9i=uarck@f(1a^m8s0j)l-7-1h-q((#$x1aq9muq%g^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
