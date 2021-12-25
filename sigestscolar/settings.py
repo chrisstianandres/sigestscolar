f68c0d3a8e481778f15792652e7698c02b703474
christian <chrisstianandres@gmail.com>
2021-12-24 20:22:05 -0500

    seet

--- sigestscolar/settings.py
+++ sigestscolar/settings.py
@@ -101,23 +101,23 @@ WSGI_APPLICATION = 'sigestscolar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'bd_scolar',
-#             'USER': 'postgres',
-#             'PASSWORD': '1234',
-#             'HOST': 'localhost',
-#             'PORT': '5432',
-#         }
-#     }
-import dj_database_url
-from decouple import config
 DATABASES = {
-    'default': {
-        'ENGINE': dj_database_url.config(default=config('DATABASE_URL')),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'bd_scolar',
+            'USER': 'postgres',
+            'PASSWORD': '1234',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+# import dj_database_url
+# from decouple import config
+# DATABASES = {
+#     'default': {
+#         'ENGINE': dj_database_url.config(default=config('DATABASE_URL')),
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
